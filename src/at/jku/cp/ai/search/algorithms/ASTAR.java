44491bb6b6a91d3997d8a49cf894f5d05055b367
Stefan <steve260897@gmail.com>
2019-10-30 10:01:22 +0100

    Over engineered the shit out of ASTAR

--- src/at/jku/cp/ai/search/algorithms/ASTAR.java
+++ src/at/jku/cp/ai/search/algorithms/ASTAR.java
@@ -6,38 +6,36 @@ import at.jku.cp.ai.search.datastructures.Pair;
 import at.jku.cp.ai.search.datastructures.StablePriorityQueue;
 
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class ASTAR implements Search {
 
-	private Function<Node, Double> heuristic;
+	private Function<Node, Double> f_heuristic, f_cost;
 
-	private Function<Node, Double> cost;
-
-	public ASTAR(final Function<Node, Double> heuristic, final Function<Node, Double> cost) {
-		this.heuristic = heuristic;
-		this.cost = cost;
+	public ASTAR(final Function<Node, Double> f_heuristic, final Function<Node, Double> f_cost) {
+		this.f_heuristic = f_heuristic;
+		this.f_cost = f_cost;
 	}
 
 	@Override
 	public Node search(final Node start, final Predicate<Node> endPredicate) {
-		final StablePriorityQueue<Costs, Node> fringe = new StablePriorityQueue<>();
-		fringe.add(new Pair<>(new Costs(heuristic.apply(start), cost.apply(start)), start));
+		final Queue<Pair<Costs, Node>> fringe = new StablePriorityQueue<>();
+		fringe.add(new Pair<>(Costs.from(f_heuristic, f_cost, start), start));
 		final Set<Node> closed = new HashSet<>();
 
-		do {
-			final Pair<Costs, Node> curr = fringe.poll();
+		for (Pair<Costs, Node> curr = fringe.poll(); curr != null; curr = fringe.poll()) {
 			if (endPredicate.test(curr.s)) return curr.s;
 			closed.add(curr.s);
 
 			for (final Node n : curr.s.adjacent()) {
 				if (!closed.contains(n)) {
-					fringe.add(new Pair<>(new Costs(heuristic.apply(n), curr.f.cost + cost.apply(n)), n));
+					fringe.add(new Pair<>(Costs.from(f_heuristic, f_cost, n, curr.f), n));
 				}
 			}
-		} while (!fringe.isEmpty());
+		}
 		return null;
 	}
 
@@ -49,9 +47,29 @@ public class ASTAR implements Search {
 			this.cost = cost;
 		}
 
+		private static Costs from(final Function<Node, Double> f_heur, final Function<Node, Double> f_cost, final Node n) {
+			return new Costs(f_heur.apply(n), f_cost.apply(n));
+		}
+
+		private static Costs from(final Function<Node, Double> f_heur, final Function<Node, Double> f_cost, final Node n, final Costs c) {
+			return new Costs(f_heur.apply(n), c.getCost() + f_cost.apply(n));
+		}
+
+		private double getHeuristic() {
+			return heuristic;
+		}
+
+		private double getCost() {
+			return cost;
+		}
+
+		private double sum() {
+			return getHeuristic() + getCost();
+		}
+
 		@Override
 		public int compareTo(final Costs other) {
-			return Double.compare(heuristic + cost, other.heuristic + other.cost);
+			return Double.compare(sum(), other.sum());
 		}
 	}
 
